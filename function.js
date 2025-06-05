f76143bdf3d1d02fcec9a4fb67cc1589632ce871
sagorsenanupam <senanupam10@gmail.com>
2025-06-05 17:39:26 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,9 @@
+const money = 45;
+
+function fanOffKor() {
+  console.log("Bosha theke uthe dara");
+  console.log("Walk towards to the switch");
+  console.log("click the switch to off");
+}
+
+function brushTeeth() {}
