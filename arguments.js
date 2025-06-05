e99164a7840413de9563f13ccbf9ee36ec0b6c65
sagorsenanupam <senanupam10@gmail.com>
2025-06-05 18:03:04 +0600

    edited

---
+++ arguments.js
@@ -0,0 +1,10 @@
+function doubleIt(number) {
+  const doubled = number * 2;
+  console.log(number, doubled);
+}
+console.log("I will call the function");
+doubleIt(15);
+console.log("------------------------");
+doubleIt(88);
+console.log("------------------------");
+doubleIt(873);
