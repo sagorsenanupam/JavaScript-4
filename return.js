a64357cf96fecf2956164520c763682b58e31ccd
sagorsenanupam <senanupam10@gmail.com>
2025-06-05 18:11:12 +0600

    edited

---
+++ return.js
@@ -0,0 +1,11 @@
+function TenTimes(number) {
+  const result = number * 10;
+  return result;
+}
+TenTimes();
+
+function cutHalf(number) {
+  const half = number / 2;
+  return half;
+}
+cutHalf();
